d4949622e631eb1d4a9c84231693337c8c1d9f2f
Dídac Sementé Fernández <didac.semente@gmail.com>
2020-09-01 09:33:26 +0200

    Added email argument Added folder zipping logic

--- backup.py
+++ backup.py
@@ -1,9 +1,12 @@
 #!/bin/python3
 import argparse
-from os.path import abspath, isdir
+from os.path import abspath, isdir, sep as pathsep, split
+from tempfile import mkdtemp
+from shutil import make_archive, rmtree
 
 import boto3
 from botocore.exceptions import ClientError
+
 
 def is_valid_bucket(bucket_name: str):
     """Checks if the bucket both exists and that we can write to it.
@@ -12,8 +15,8 @@ def is_valid_bucket(bucket_name: str):
         bucket_name (str): Bucket to check
 
     Returns:
-        Optional[s3.Bucket]: True if the bucket named 'bucket_name' exists and 
-            can be written to, return False otherwise.
+        Optional[s3.Bucket]: The Bucket with 'bucket_name' if it exists and
+            is writtable, None otherwise.
     """
 
     s3 = boto3.resource('s3')
@@ -24,6 +27,7 @@ def is_valid_bucket(bucket_name: str):
     except ClientError as e:
         return None
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Backs up the given list of folders to the target S3 bucket')
@@ -32,10 +36,12 @@ if __name__ == "__main__":
                         help='Target S3 bucket to which back up the folder')
     parser.add_argument('folder', nargs='+',
                         help='Folder to back up')
-    parser.add_argument('-ts', '--timestamp', 
+    parser.add_argument('-ts', '--timestamp',
                         help='Add timestamp suffix to the uploaded files name?',
                         action='store_true')
-    # TODO: Add email argument
+    parser.add_argument('-e', '--email', 
+                        help='Send email on process completed',
+                        action='store_true')
 
     parsed_args, _ = parser.parse_known_args()
 
@@ -46,7 +52,20 @@ if __name__ == "__main__":
     # Bucket validation
     if bucket := is_valid_bucket(bucket_name):
         # Folder filtering
-        folder_list = [f for f in folder_list if isdir(abspath(f))]
+        folder_list = [abspath(f) for f in folder_list if isdir(abspath(f))]
+
+        if not folder_list:
+            raise ValueError('None of the supplied folder paths are valid!')
+
+        base_tmp_dir = mkdtemp()
+
+        for folder in folder_list:
+            _, folder_name = split(folder)
+
+            out_path = f'{base_tmp_dir}{pathsep}{folder_name}'
+            result = make_archive(base_dir='.', root_dir=folder, format='zip', base_name=out_path)
+
+            print(result)
 
         # TODO: For each valid folder we will compress it in a temp location,
         # add a timestamp if provided and upload to S3, if any errors
@@ -54,5 +73,3 @@ if __name__ == "__main__":
         # an error email.
     else:
         raise ValueError(f'Invalid target bucket {bucket_name}!')
-
-    print(bucket_name, folder_list, add_timestamp)
